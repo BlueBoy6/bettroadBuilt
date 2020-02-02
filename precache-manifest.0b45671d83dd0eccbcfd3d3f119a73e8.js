538d7d0108c07b641a8dcb14040ebfc1f5ad5ca2
xubuntuPartVM <xubuntuPartVM@gnagna.fr>
2020-02-02 16:35:44 +0100

    release bettroad built

---
+++ precache-manifest.0b45671d83dd0eccbcfd3d3f119a73e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e1aa4267f06ba26a72d65b55a08b65",
+    "url": "./index.html"
+  },
+  {
+    "revision": "128b3a928cc24f4bdc4e",
+    "url": "./static/css/main.0d5217e3.chunk.css"
+  },
+  {
+    "revision": "8992daddbff730e656ef",
+    "url": "./static/js/2.3f9999e8.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "./static/js/2.3f9999e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "128b3a928cc24f4bdc4e",
+    "url": "./static/js/main.7b453716.chunk.js"
+  },
+  {
+    "revision": "40be310bb6028c08825c",
+    "url": "./static/js/runtime-main.2edb1291.js"
+  }
+]);
